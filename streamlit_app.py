7926ad51a481596e998a68de0dc8a18d86626400
Dhruv <113442073+dp808139@users.noreply.github.com>
2022-09-14 10:04:50 +0530

    Update streamlit_app.py
    menu updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & rocket smoohtie')
-streamli.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')

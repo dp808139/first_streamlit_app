6b385bf70a6c7875d2d5350290c40afe0c529467
Dhruv <113442073+dp808139@users.noreply.github.com>
2022-09-14 10:08:57 +0530

    Update streamlit_app.py
    Header changed and emoji added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,9 @@ import streamlit
 streamlit.title('Hello World! Dhruv Here...')
 
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & rocket smoohtie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & rocket smoohtie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Aevocado Toast')
